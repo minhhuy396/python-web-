2cf870f09bca5e0974ddf819da33930909011eb3
Nguyen Minh Huy <nguyen.huy2006@icloud.com>
2026-03-25 18:13:08 +0700

    sua

--- shopweb/settings.py
+++ shopweb/settings.py
@@ -134,13 +134,17 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nmhuy396@gmail.com'
-EMAIL_HOST_PASSWORD = 'xwhueauhyatstldf'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+
+DEFAULT_FROM_EMAIL = 'your_email@example.com'
 import cloudinary
 
 cloudinary.config(
